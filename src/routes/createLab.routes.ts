a2e08d48d3e2d3628e6d45ca83d353f8231a30f2
sujaykumar <82111261+sujaykumar007@users.noreply.github.com>
2025-04-22 04:24:11 +0530

    Run the prisma generate

--- src/routes/createLab.routes.ts
+++ src/routes/createLab.routes.ts
@@ -3,5 +3,5 @@ import { Router } from "express";
 import { verifyAdminToken } from "../middleware/auth.jwt";
 import { createLab } from "../controllers/createlab.components";
 const router = Router();
-router.post("/create",verifyAdminToken, createLab)
+router.post("/create",verifyAdminToken, createLab);
 export default router;
